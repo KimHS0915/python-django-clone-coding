1cbce9dfaf3816abd7cfd09cb1899dd0d2feef2b
KimHS0915 <remember2552@gmail.com>
2021-11-14 01:10:14 +0900

    Update UserAdmin and User

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(UserAdmin):
+    """ Custom User Admin """
+    
+    custom_fieldsets = (
+        ('Custom', {
+            "fields": (
+                'avatar', 
+                'bio', 
+                'birthdate', 
+                'currency', 
+                'gender', 
+                'language', 
+                'superhost',
+            ),
+        }),
+    )
+    fieldsets = UserAdmin.fieldsets + custom_fieldsets
+    
