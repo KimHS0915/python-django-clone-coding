0665dde1aac9826f45cdeda28f0e761b4331cb2c
KimHS0915 <remember2552@gmail.com>
2021-12-07 22:56:53 +0900

    Refactoring: fbv to cbv

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,5 @@
-from django.http.response import Http404
 from django.utils import timezone
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -22,11 +20,7 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, 'rooms/detail.html', context={
-            'room': room,
-        })
-    except models.Room.DoesNotExist:
-        raise Http404
+class RoomDetailView(DetailView):
+    """ Room Detail Definition """
+
+    model = models.Room
