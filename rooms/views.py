a11f17bce9cdfe792cc9b08a6f5d1874d0155eea
KimHS0915 <remember2552@gmail.com>
2021-12-08 18:54:48 +0900

    Update Search
    Add country, room_type, price, guests, bedrooms, beds, baths, instant,
    super_host, amenities, facilities

--- rooms/views.py
+++ rooms/views.py
@@ -31,9 +31,42 @@ class RoomDetailView(DetailView):
 def search(request):
     city = request.GET.get('city')
     city = city.capitalize() if city else ''
-    room_types = models.RoomType.objects.all()
-    return render(request, "rooms/search.html", context={
+    selected_country = request.GET.get('country', 'KR')
+    selected_room_type = int(request.GET.get('room_type', 0))
+    price = int(request.GET.get('price', 0))
+    guests = int(request.GET.get('guests', 0))
+    bedrooms = int(request.GET.get('bedrooms', 0))
+    beds = int(request.GET.get('beds', 0))
+    baths = int(request.GET.get('baths', 0))
+    instant = request.GET.get('instant', False)
+    super_host = request.GET.get('super_host', False)
+    selected_amenities = request.GET.getlist('amenities')
+    selected_facilities = request.GET.getlist('facilities')
+
+    form = {
         'city': city,
+        'selected_room_type': selected_room_type,
+        'selected_country': selected_country,
+        'price': price,
+        'guests': guests,
+        'bedrooms': bedrooms,
+        'beds': beds,
+        'baths': baths,
+        'instant': instant,
+        'super_host': super_host,
+        'selected_amenities': selected_amenities,
+        'selected_facilities': selected_facilities,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
         'countries': countries,
         'room_types': room_types,
-    })
+        'amenities': amenities,
+        'facilities': facilities,
+    }
+
+    return render(request, "rooms/search.html", context={**form, **choices})
