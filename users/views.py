575b72624dd61a6ab6a2b73ad53f9b9d34c7545d
KimHS0915 <remember2552@gmail.com>
2021-12-13 17:37:52 +0900

    Update login
    Add validation and authentication
    Add log out

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
+from django.contrib import auth
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
@@ -10,4 +13,17 @@ class LoginView(View):
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=email, password=password)
+            if user:
+                login(request, user)
+                return redirect(reverse('common:home'))
+
         return render(request, 'users/login.html', context={'form': form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse('common:home'))
