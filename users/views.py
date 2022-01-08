b05638bbc4c5b0e8e41c0627a0709276d2367712
KimHS0915 <remember2552@gmail.com>
2022-01-08 22:47:30 +0900

    Add EmailLoginOnlyView, LoggedInOnlyView

--- users/views.py
+++ users/views.py
@@ -16,7 +16,6 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = 'users/login.html'
     form_class = forms.LoginForm
-    success_url = reverse_lazy('common:home')
 
     def form_valid(self, form):
         email = form.cleaned_data.get('email')
@@ -26,6 +25,13 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
             login(self.request, user)
             messages.success(self.request, f'Welcome back {user.first_name}')
         return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get('next')
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse('common:home')
 
 
 def log_out(request):
@@ -201,7 +207,7 @@ class UserProfileView(DetailView):
     context_object_name = 'user_obj'
 
 
-class UpdateProfileView(SuccessMessageMixin, UpdateView):
+class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
 
     models = models.User
     template_name = 'users/update_profile.html'
@@ -237,7 +243,12 @@ class UpdateProfileView(SuccessMessageMixin, UpdateView):
         return form
 
 
-class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
+class UpdatePasswordView(
+    mixins.LoggedInOnlyView,
+    mixins.EmailLoginOnlyView,
+    SuccessMessageMixin,
+    PasswordChangeView
+):
 
     template_name = 'users/update_password.html'
     success_message = "Password Updated"
