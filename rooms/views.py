b29931306453ef1ac842faf243b9bd68efbaf57b
KimHS0915 <remember2552@gmail.com>
2022-01-11 22:59:44 +0900

    Add edit photo

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from django.contrib.messages.api import success
 from django.http.response import Http404
 from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView, View
@@ -6,6 +7,7 @@ from django.urls import reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from users import mixins as user_mixins
 from . import models, forms
 
@@ -161,3 +163,22 @@ def delete_photo(request, room_pk, photo_pk):
         return redirect(reverse('rooms:photos', kwargs={'pk': room_pk}))
     except models.Room.DoesNotExist:
         return redirect(reverse('common:home'))
+
+
+class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
+
+    model = models.Photo
+    template_name = 'rooms/photo_edit.html'
+    pk_url_kwarg = 'photo_pk'
+    success_message = 'Photo Updated'
+    fields = ('caption',)
+
+    def get_success_url(self):
+        room_pk = self.kwargs.get('room_pk')
+        return reverse('rooms:photos', kwargs={'pk': room_pk})
+
+    def get_object(self, queryset=None):
+        photo = super().get_object(queryset=queryset)
+        if photo.room.host.pk != self.request.user.pk:
+            raise Http404()
+        return photo
