8d08cf2c8bbe88508f94c49ad1eb7ab87a6c0f76
KimHS0915 <remember2552@gmail.com>
2022-02-06 01:38:02 +0900

    Update favourites

--- lists/views.py
+++ lists/views.py
@@ -11,18 +11,33 @@ from . import models
 def toggle_room(request, room_pk):
     action = request.GET.get('action', None)
     room = Room.objects.get_or_none(pk=room_pk)
+    return_addr = reverse('common:home')
     if room is not None and action is not None:
         the_list, _ = models.List.objects.get_or_create(
             user=request.user, name='My Favourites Houses')
         if action == 'add':
             the_list.rooms.add(room)
             messages.success(request, gettext("Favourites added"))
-        elif action == 'remove' or action == 'delete':
+        elif action == 'remove':
             the_list.rooms.remove(room)
             messages.success(request, gettext('Favourites removed'))
-            if action == 'delete':
-                return redirect(reverse('lists:see-favs'))
-    return redirect(reverse('rooms:detail', kwargs={'pk': room_pk}))
+        elif action == 'add1':
+            the_list.rooms.add(room)
+            messages.success(request, gettext("Favourites added"))
+            return_addr = reverse('rooms:detail', kwargs={'pk': room_pk})
+        elif action == 'remove1':
+            the_list.rooms.remove(room)
+            messages.success(request, gettext('Favourites removed'))
+            return_addr = reverse('rooms:detail', kwargs={'pk': room_pk})
+        elif action == 'add2':
+            the_list.rooms.add(room)
+            messages.success(request, gettext("Favourites added"))
+            return_addr = reverse('lists:see-favs')
+        elif action == 'remove2':
+            the_list.rooms.remove(room)
+            messages.success(request, gettext('Favourites removed'))
+            return_addr = reverse('lists:see-favs')
+    return redirect(return_addr)
 
 
 class SeeFavouritesView(TemplateView):
