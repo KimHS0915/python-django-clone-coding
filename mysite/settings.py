d1d7140d9f20c172da37383280e1d39ef6fc9cc7
KimHS0915 <remember2552@gmail.com>
2022-02-19 01:16:11 +0900

    Fix static settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -193,14 +193,17 @@ CURRENCY_COOKIE_NAME = 'django_currency'
 
 if not DEBUG:
 
+    # AWS S3
+
     DEFAULT_FILE_STORAGE = 'mysite.custom_storages.UploadStorage'
     STATICFILES_STORAGE = 'mysite.custom_storages.StaticStorage'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
 
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
     # Sentry
 
